6e99a2d63088d088cdb50630ae7309cce558adf9
Maria Johansson <jarventaus@hotmail.com>
2022-02-24 15:25:18 +0100

    Merge branch 'Frontend'

--- Frontend/FrontendAuthenticationDemo/src/components/Footer.tsx
+++ Frontend/FrontendAuthenticationDemo/src/components/Footer.tsx
@@ -2,8 +2,6 @@ import React from "react"
 import { Container } from "../Styles/Container.styled"
 import { Flex } from "../styles/Flex.styled"
 import { StyledFooter } from "../Styles/Footer.styled"
-import { FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa"
-import { StyledSocialIcons } from "../Styles/Socials.styled"
 
 function Footer() {
   return (
@@ -11,42 +9,21 @@ function Footer() {
       <Container>
         <Flex>
           <ul>
-            <li>
-              <h1>Demo</h1>
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliquaddss
-            </li>
-            <li>+1-000-000-000</li>
-            <li>example@demo.com</li>
+            <li></li>
           </ul>
           <ul>
-            <li>About Us</li>
-            <li>What We Do</li>
-            <li>FAQ</li>
+            <li></li>
+            <li></li>
           </ul>
-
           <ul>
-            <li>Career</li>
-            <li>Blog</li>
-            <li>Contact Us</li>
+            <li>
+              &copy; VT 2022, Grupp 17. All rights reserved. &nbsp;
+              <a href="https://sup2022teamafry.wordpress.com">
+                sup2022teamafry.wordpress.com
+              </a>
+            </li>
           </ul>
-
-          <StyledSocialIcons>
-            <a href="https://twitter.com">
-              <FaTwitter />
-            </a>
-            <a href="https://facebook.com">
-              <FaFacebook />
-            </a>
-            <a href="https://linkedin.com">
-              <FaLinkedin />
-            </a>
-          </StyledSocialIcons>
         </Flex>
-
-        <p>&copy; 2022 Demo. All rights reserved.</p>
       </Container>
     </StyledFooter>
   )
--- Frontend/FrontendAuthenticationDemo/src/styles/Container.styled.tsx
+++ Frontend/FrontendAuthenticationDemo/src/styles/Container.styled.tsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Container = styled.div`
   width: 1600px;
   max-width: 100%;
   padding: 0 20px;
   margin: 0 auto;
-  color: ${({theme}) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.text};
   @media (max-width: ${({ theme }) => theme.responsive.sm}) {
     padding: 0 10px;
   }
@@ -14,9 +14,8 @@ export const Container = styled.div`
 export const Page = styled.div`
   width: 100%;
   max-width: 100%;
-  min-height: 100vh;
+  min-height: 70vh;
   padding: 20px 10px 20px 10px;
   margin: 20px 0 auto;
-  color: ${({theme}) => theme.colors.text};
-
+  color: ${({ theme }) => theme.colors.text};
 `
