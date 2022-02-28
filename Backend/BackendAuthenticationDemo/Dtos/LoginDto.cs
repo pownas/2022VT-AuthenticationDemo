c0f3f34b77b9df2ed6eeb8b0ca5ab02ad21ae2fa
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-02-28 11:00:14 +0100

    Update LoginDto.cs

--- Backend/BackendAuthenticationDemo/Dtos/LoginDto.cs
+++ Backend/BackendAuthenticationDemo/Dtos/LoginDto.cs
@@ -6,10 +6,10 @@ namespace BackendAuthenticationDemo.Dtos;
 public class LoginDto
 {
     [Required]
-    [DefaultValue("system")]
+    [MinLength(2)]
     public string UserName { get; set; } = string.Empty;
 
     [Required]
-    [DefaultValue("DemoPassword1!")]
+    [MinLength(8)]
     public string Password { get; set; } = string.Empty;
 }
